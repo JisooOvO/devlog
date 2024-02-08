01d5c54ef575943cb3f9b81c701b1c1cea9481fa
fdking1887 <fdking1887@naver.com>
2024-02-08 21:27:34 +0900

    router test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./common/Header";
 import Main from "./router/Main";
+import About from "./router/About";
 import Footer from "./common/Footer";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </BrowserRouter>
