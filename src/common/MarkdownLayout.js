b2bf21132c9625d0754f74b47a0293cc63f9fb4a
fdking1887 <fdking1887@naver.com>
2024-02-13 21:50:33 +0900

    # 마크다운 CSS 수정

--- src/common/MarkdownLayout.js
+++ src/common/MarkdownLayout.js
@@ -1,25 +1,53 @@
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { prism } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 import "../markdown.css";
+
+function countSpaces(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") count++;
+    else break;
+  }
+
+  return count / 2;
+}
 
 const MarkdownLayout = ({ contents }) => {
   const components = {
     code: ({ node, inline, className, children, ...props }) => {
-      const match = /language-(\w+)/.exec(className || "");
-      return !inline && match ? (
-        <SyntaxHighlighter
-          language={match[1]}
-          style={prism}
-          PreTag="div"
-          {...props}
-        >
-          {String(children).replace(/\n$/, "")}
-        </SyntaxHighlighter>
-      ) : (
+      const pList = children.split("\n");
+      const newChildren = pList.map((p, idx) => {
+        const classLists = [];
+        const tab = countSpaces(p);
+
+        if (tab !== 0) {
+          classLists.push("tab-span");
+          classLists.push(`tab${tab}`);
+        }
+
+        if (
+          String(p).trim().startsWith("//") ||
+          String(p).trim().startsWith("...")
+        ) {
+          classLists.push("annotation");
+        }
+
+        return (
+          <span
+            key={idx}
+            style={{
+              marginLeft: classLists.includes("tab-span") ? `${15 * tab}px` : 0,
+            }}
+            className={classLists.join(" ")}
+          >
+            {p}
+          </span>
+        );
+      });
+      return (
         <code className={className} {...props}>
-          {children}
+          {newChildren}
         </code>
       );
     },
